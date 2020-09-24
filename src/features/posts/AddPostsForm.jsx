f9730ca4251dc02bdb79a47cbd3187e67a5ef8df
sunk0 <sunk0@abv.bg>
2020-09-24 18:24:16 +0300

    ffs

---
+++ src/features/posts/AddPostsForm.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react'
+import {useDispatch} from 'react-redux'
+import {nanoid} from '@reduxjs/toolkit'
+import {postAdded} from './postsSlice'
+
+export const AddPostsForm = () => {
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+
+    const dispatch = useDispatch();
+
+    const onTitleChanged = e => setTitle(e.target.value);
+    const onContentChanged = e => setContent(e.target.value);
+
+    const onSavePostClicked = () => {
+        if(title && content) {
+            dispatch(
+                postAdded({
+                    id:nanoid(),
+                    title,
+                    content
+                })
+            )
+        }
+    }
+
+    return (
+        <section>
+        <h2>Add New Post</h2>
+        <form>
+            <label htmlFor="postTitle">Post title:</label>
+            <input
+            type="text"
+            id="postTitle"
+            name="postTitle"
+            value={title}
+            onChange={onTitleChanged}
+            ></input>
+            <label htmlFor="postContent">Content:</label>
+            <textarea
+            id="postContent"
+            name="postContent"
+            value={content}
+            onChange={onContentChanged}
+            >
+            </textarea>
+            <button
+            type="button"
+            onClick={onSavePostClicked}
+            >
+                Save Post
+            </button>
+        </form>
+        </section>
+    )
+}
